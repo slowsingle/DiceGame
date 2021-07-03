cb51825f721f76fe82b7c4da19cad45237db5ccf
UMA <ume0kanaz@gmail.com>
2021-07-03 23:04:29 +0900

    rename

--- Assets/Private/Scripts/PlayingCardsController.cs
+++ Assets/Private/Scripts/PlayingCardsController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class PlayingCardsTest : MonoBehaviour
+public class PlayingCardsController : MonoBehaviour
 {
     [SerializeField] private Transform basePoint;
 
